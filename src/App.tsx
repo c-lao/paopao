9c9f45621a29d3c82d3f32d0ffb38d4d74e0deb1
kun <hannibalhalifax950@outlook.com>
2025-09-21 23:09:25 +0800

    v0.5.0

--- src/App.tsx
+++ src/App.tsx
@@ -101,7 +101,7 @@ function HeroSection() {
               <img
                 onClick={() => {
                   window.open(
-                    'https://s3.ap-southeast-1.amazonaws.com/app.paoscrm.com/bubbles-0.4.6-setup.exe',
+                    'https://s3.ap-southeast-1.amazonaws.com/app.paoscrm.com/bubbles-0.5.0-setup.exe',
                     '_blank'
                   );
                 }}
