2ed01d488c7bc0cc38df61f426f3f1718308dbaf
kun <hannibalhalifax950@outlook.com>
2025-12-23 01:34:17 +0800

    chore: 0.7.4

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,4 +2,4 @@ export const REGISTER_URL = 'https://paoscrm.com/#/auth/register'
 
 export const LOGIN_URL = 'https://paoscrm.com/#/'
 
-export const DOWNLOAD_URL = 'https://s3.ap-southeast-1.amazonaws.com/app.paoscrm.com/bubbles-0.7.3-setup.exe'
+export const DOWNLOAD_URL = 'https://s3.ap-southeast-1.amazonaws.com/app.paoscrm.com/bubbles-0.7.4-setup.exe'
